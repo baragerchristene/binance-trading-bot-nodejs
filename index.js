53519a4c1ad508eae8522995744a6c28ea8ded7f
Oğuzhan Kurum <59623554+oguzhankurumm@users.noreply.github.com>
2023-03-25 23:16:26 +0300

    editing index file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const Binance = require('node-binance-api');
 const binance = new Binance().options({
-    APIKEY: 'YOUR_KEY', // change this to your API key
-    APISECRET: 'YOUR_SECRET', // change this to your API secret
+    APIKEY: 'YOUR_KEY', // change this with your API key
+    APISECRET: 'YOUR_SECRET', // change this with your API secret
     useServerTime: true, // If you get timestamp errors, synchronize with server time at startup
     test: false // If you want to use the testnet, set this to true
 });
